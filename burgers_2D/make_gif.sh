01d6b44d1d45a334be00c59c178a612fc7935fec
Paht J <paht.juangphanich@gmail.com>
2022-01-09 16:21:56 -0500

    updated gif

--- burgers_2D/make_gif.sh
+++ burgers_2D/make_gif.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Install imagemagick if not there
 # sudo apt-get install imagemagick
-convert -resize 20% -delay 20 -loop 0 figures/analytical*.png analytical.gif
-convert -resize 20% -delay 20 -loop 0 figures/ml*.png ml.gif
+convert -resize 15% -delay 10 -loop 0 figures/analytical*.png analytical.gif
+convert -resize 15% -delay 10 -loop 0 figures/ml*.png ml.gif
